d846a71896512d5700e77e8e62494d11f36ba9ae
jgellatly5 <jgellatly5@gmail.com>
2018-08-05 19:58:11 -0700

    Getting text from TextView on selected item in settings
    I need to get the image and set its visibility with the appropriate
    marker

--- app/src/main/java/com/gallopdevs/athanhelper/settings/SettingsFragment.java
+++ app/src/main/java/com/gallopdevs/athanhelper/settings/SettingsFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gallopdevs.athanhelper.R;
 
@@ -20,6 +24,8 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class SettingsFragment extends Fragment {
+
+    private static final String TAG = "SettingsFragment";
 
     @BindView(R.id.expandable_list_view)
     ExpandableListView expandableListView;
@@ -43,7 +49,29 @@ public class SettingsFragment extends Fragment {
         prepareListData();
 
         adapter = new CustomExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
+
+//        Display newDisplay = getActivity().getWindowManager().getDefaultDisplay();
+//        int width = newDisplay.getWidth();
+//        expandableListView.setIndicatorBounds(width - 100, width);
+
         expandableListView.setAdapter(adapter);
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int groupPosition, int childPosition, long id) {
+
+                Log.d(TAG, "onChildClick: clicked");
+                View convertView = LayoutInflater.from(getActivity()).inflate(R.layout.list_settings_items, null);
+                View childView = adapter.getChildView(groupPosition, childPosition, false, convertView, null);
+//                ImageView indicator = childView.findViewById(R.id.selection_indicator);
+//                Log.d(TAG, "onChildClick: indicator: " + indicator);
+////                indicator.setVisibility(ImageView.INVISIBLE);
+                TextView textView = childView.findViewById(R.id.item);
+                String text = textView.getText().toString();
+                Log.d(TAG, "onChildClick: " + text);
+
+                return false;
+            }
+        });
 
 
         return view;
