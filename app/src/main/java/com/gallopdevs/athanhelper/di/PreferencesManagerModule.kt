9f7aef6cd073ffd2d2eaed8efbee95751a5d2331
Jordan Gellatly <jgellatly5@gmail.com>
2023-12-23 07:34:52 -0800

    Refactor di package.

--- app/src/main/java/com/gallopdevs/athanhelper/di/PreferencesManagerModule.kt
+++ app/src/main/java/com/gallopdevs/athanhelper/di/PreferencesManagerModule.kt
@@ -1,18 +1,16 @@
 package com.gallopdevs.athanhelper.di
 
-import android.content.Context
 import com.gallopdevs.athanhelper.settings.PreferencesManager
 import com.gallopdevs.athanhelper.settings.PreferencesManagerImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object PreferencesManagerModule {
+abstract class PreferencesManagerModule {
 
-    @Provides
-    fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager = PreferencesManagerImpl(context)
+    @Binds
+    abstract fun bindPreferencesManager(preferencesManagerImpl: PreferencesManagerImpl): PreferencesManager
 }
