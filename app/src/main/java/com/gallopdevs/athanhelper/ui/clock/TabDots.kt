0b9fe97229a5f3583f014f2fc5b49847692556a8
Jordan Gellatly <jgellatly5@gmail.com>
2024-01-04 15:01:32 -0800

    Showing icon in tab layout.

--- app/src/main/java/com/gallopdevs/athanhelper/ui/clock/TabDots.kt
+++ app/src/main/java/com/gallopdevs/athanhelper/ui/clock/TabDots.kt
@@ -1,33 +1,57 @@
 package com.gallopdevs.athanhelper.ui.clock
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gallopdevs.athanhelper.R
 import com.gallopdevs.athanhelper.ui.theme.AthanHelperTheme
 
 @Composable
+private fun TabImage() {
+    Image(
+        painterResource(id = R.drawable.grey_oval), contentDescription = null,
+        modifier = Modifier
+            .size(10.dp)
+            .background(
+                color = Color.Black,
+                shape = CircleShape
+            )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TabImagePreview() {
+    AthanHelperTheme {
+        TabImage()
+    }
+}
+
+@Composable
 private fun TabElement(index: Int, selectedTabIndex: Int, onTabSelected: (Int) -> Unit) {
     Tab(
-        text = { Text(text = "") },
+        icon = { TabImage() },
         selected = index == selectedTabIndex,
-//        icon = painterResource(id = R.drawable.sum_icon),
         onClick = { onTabSelected(index) },
         modifier = Modifier
             .background(colorResource(id = R.color.colorPrimary))
@@ -71,7 +95,8 @@ fun TabDots(state: PagerState) {
 @Composable
 private fun TabDotsPreview() {
     AthanHelperTheme {
-        val pagerState = rememberPagerState(initialPage = 0, pageCount = { ClockScreenConstants.NUM_ITEMS })
+        val pagerState =
+            rememberPagerState(initialPage = 0, pageCount = { ClockScreenConstants.NUM_ITEMS })
         TabDots(state = pagerState)
     }
 }
