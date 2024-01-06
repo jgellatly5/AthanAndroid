05ede265e2d21432bcba01af8e0180d83e6520bc
Jordan Gellatly <jgellatly5@gmail.com>
2024-01-05 19:23:45 -0800

    Removing fragments.

--- app/src/main/java/com/gallopdevs/athanhelper/MainActivity.kt
+++ app/src/main/java/com/gallopdevs/athanhelper/MainActivity.kt
@@ -11,39 +11,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
-import com.gallopdevs.athanhelper.MainActivity.Companion.NUM_ITEMS
 import com.gallopdevs.athanhelper.data.PrayerCalculatorIpml.Companion.JAFARI
 import com.gallopdevs.athanhelper.data.PrayerCalculatorIpml.Companion.MIDNIGHT
 import com.gallopdevs.athanhelper.data.PrayerCalculatorIpml.Companion.SHAFII
 import com.gallopdevs.athanhelper.data.PreferencesManagerImpl.Companion.ASR_METHOD
 import com.gallopdevs.athanhelper.data.PreferencesManagerImpl.Companion.CALCULATION_METHOD
 import com.gallopdevs.athanhelper.data.PreferencesManagerImpl.Companion.LATITUDES_METHOD
-import com.gallopdevs.athanhelper.ui.clock.ClockScreen
-import com.gallopdevs.athanhelper.ui.settings.SettingsScreen
-import com.gallopdevs.athanhelper.ui.theme.AthanHelperTheme
 import com.gallopdevs.athanhelper.viewmodel.ClockViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -67,7 +41,7 @@ class MainActivity : ComponentActivity() {
                             if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
                                 Toast.makeText(
                                     this@MainActivity,
-                                    "Location permissions denied.",
+                                    getString(R.string.location_permissions_denied),
                                     Toast.LENGTH_SHORT
                                 ).show()
                             }
@@ -137,14 +111,14 @@ class MainActivity : ComponentActivity() {
                 } else {
                     Toast.makeText(
                         this@MainActivity,
-                        "We cannot find your location. Please enable in settings.",
+                        getString(R.string.cannot_find_location_message),
                         Toast.LENGTH_SHORT
                     ).show()
                 }
             }.addOnFailureListener {
                 Toast.makeText(
                     this@MainActivity,
-                    "We cannot find your location. Please enable in settings.",
+                    getString(R.string.cannot_find_location_message),
                     Toast.LENGTH_SHORT
                 ).show()
             }
@@ -153,79 +127,5 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         const val CHANNEL_ID = "NOTIFICATION"
-        const val NUM_ITEMS = 2
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun MainPager() {
-    var selectedTabIndex by remember { mutableIntStateOf(0) }
-    val pagerState = rememberPagerState(initialPage = 0, pageCount = { NUM_ITEMS })
-    Column {
-        HorizontalPager(state = pagerState, modifier = Modifier.weight(1f)) {
-            when (it) {
-                0 -> ClockScreen()
-                1 -> SettingsScreen()
-            }
-        }
-        TabRow(
-            selectedTabIndex = selectedTabIndex,
-            indicator = { tabPositions ->
-                // Custom indicator
-                TabRowDefaults.Indicator(
-                    Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
-                    color = colorResource(id = R.color.colorPrimaryDark)
-                )
-            }
-        ) {
-            for (i in 0 until NUM_ITEMS) {
-                TabElement(index = i, selectedTabIndex = selectedTabIndex) {
-                    selectedTabIndex = it
-                }
-            }
-        }
-    }
-
-}
-
-@Composable
-private fun TabElement(
-    index: Int,
-    selectedTabIndex: Int,
-    onTabSelected: (Int) -> Unit
-) {
-    Tab(
-        icon = { TabIcon(index) },
-        selected = index == selectedTabIndex,
-        onClick = { onTabSelected(index) },
-        modifier = Modifier
-            .background(colorResource(id = R.color.colorPrimary))
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-    )
-}
-
-@Composable
-private fun TabIcon(index: Int) {
-    val painterId = when (index) {
-        0 -> R.drawable.clock_icon
-        else -> R.drawable.settings_icon
-    }
-    val contentDescription = when (index) {
-        0 -> "clock"
-        else -> "settings"
-    }
-    Image(
-        painterResource(id = painterId),
-        contentDescription = contentDescription,
-        modifier = Modifier.size(30.dp)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MainPagerPreview() {
-    AthanHelperTheme {
-        MainPager()
     }
 }
---
+++ app/src/main/java/com/gallopdevs/athanhelper/MainPager.kt
@@ -0,0 +1,124 @@
+package com.gallopdevs.athanhelper
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gallopdevs.athanhelper.MainPagerConstants.NUM_ITEMS
+import com.gallopdevs.athanhelper.ui.clock.ClockScreen
+import com.gallopdevs.athanhelper.ui.settings.SettingsScreen
+import com.gallopdevs.athanhelper.ui.theme.AthanHelperTheme
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainPager() {
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { NUM_ITEMS })
+    Column {
+        HorizontalPager(state = pagerState, modifier = Modifier.weight(1f)) {
+            when (it) {
+                0 -> ClockScreen()
+                1 -> SettingsScreen()
+            }
+        }
+        TabRow(
+            selectedTabIndex = selectedTabIndex,
+            indicator = { tabPositions ->
+                // Custom indicator
+                TabRowDefaults.Indicator(
+                    Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                    color = colorResource(id = R.color.colorPrimaryDark)
+                )
+            }
+        ) {
+            for (i in 0 until NUM_ITEMS) {
+                TabElement(index = i, selectedTabIndex = selectedTabIndex) {
+                    selectedTabIndex = it
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainPagerPreview() {
+    AthanHelperTheme {
+        MainPager()
+    }
+}
+
+@Composable
+private fun TabElement(
+    index: Int,
+    selectedTabIndex: Int,
+    onTabSelected: (Int) -> Unit
+) {
+    Tab(
+        icon = { TabIcon(index) },
+        selected = index == selectedTabIndex,
+        onClick = { onTabSelected(index) },
+        modifier = Modifier
+            .background(colorResource(id = R.color.colorPrimary))
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TabElementPreview() {
+    AthanHelperTheme {
+        TabElement(
+            index = 0,
+            selectedTabIndex = 0,
+            onTabSelected = {}
+        )
+    }
+}
+
+@Composable
+private fun TabIcon(index: Int) {
+    val painterId = when (index) {
+        0 -> R.drawable.clock_icon
+        else -> R.drawable.settings_icon
+    }
+    val contentDescription = when (index) {
+        0 -> "clock"
+        else -> "settings"
+    }
+    Image(
+        painterResource(id = painterId),
+        contentDescription = contentDescription,
+        modifier = Modifier.size(30.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TabIconPreview() {
+    AthanHelperTheme {
+        TabIcon(index = 0)
+    }
+}
+
+object MainPagerConstants {
+    const val NUM_ITEMS = 2
+}
